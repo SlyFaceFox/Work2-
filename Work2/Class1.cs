1eed0a3b365de926016e0baf8257649a77fc832e
Huawei <Huawei@LAPTOP-5LRHA01L>
2023-02-10 10:22:07 +0300

    Добавьте файлы проекта.

---
+++ Work2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace Work2
+{
+    public class Buildings
+    {
+        private int height1;
+        private int height2;
+        private int height3;
+
+        public int Height1 { get; set; }
+        public int Height2 { get; set; }
+        public int Height3 { get; set; }
+
+        public Buildings(int height1, int height2, int height3)
+        {
+            Height1 = height1;
+            Height2 = height2;
+            Height3 = height3;
+        }
+
+        public override string ToString()
+        {
+            return $"There are three buildings on the street. One is {Height1} meters in high, the second is {Height2} meters, the third is {Height3} meters high";
+        }
+    }
+}
---
+++ Work2/Program.cs
@@ -0,0 +1,10 @@
+﻿using Work2;
+
+Buildings[] height = new Buildings[5];
+Random rand = new Random();
+
+for (int i = 0; i < height.Length; i++)
+{
+    height[i] = new Buildings(rand.Next(15, 300), rand.Next(15, 300), rand.Next(15, 300));
+    Console.WriteLine(height[i]);
+}
